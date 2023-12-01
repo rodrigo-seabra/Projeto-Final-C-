60c4dd05c5e6624964cc81d0257aaaf22f9c9a01
rodrigoseabra1 <rodrigo.seabra0106@hotmail.com>
2023-12-01 17:57:00 -0300

    Criação controllers

---
+++ projeto-final/Controllers/EntradaProdutoController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using projeto_final.Models;
+
+namespace projeto_final.Controllers
+{
+    public class EntradaProdutoController : Controller
+    {
+        private readonly Contexto _context;
+
+        public EntradaProdutoController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: EntradaProduto
+        public async Task<IActionResult> Index()
+        {
+            var contexto = _context.EntradaProduto.Include(e => e.Produto);
+            return View(await contexto.ToListAsync());
+        }
+
+        // GET: EntradaProduto/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.EntradaProduto == null)
+            {
+                return NotFound();
+            }
+
+            var entradaProduto = await _context.EntradaProduto
+                .Include(e => e.Produto)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (entradaProduto == null)
+            {
+                return NotFound();
+            }
+
+            return View(entradaProduto);
+        }
+
+        // GET: EntradaProduto/Create
+        public IActionResult Create()
+        {
+            ViewData["ProdutoId"] = new SelectList(_context.Produto, "Id", "Id");
+            return View();
+        }
+
+        // POST: EntradaProduto/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,EntradaProdutoQuantidade,EntradaProdutoData,ProdutoId")] EntradaProduto entradaProduto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(entradaProduto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProdutoId"] = new SelectList(_context.Produto, "Id", "Id", entradaProduto.ProdutoId);
+            return View(entradaProduto);
+        }
+
+        // GET: EntradaProduto/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.EntradaProduto == null)
+            {
+                return NotFound();
+            }
+
+            var entradaProduto = await _context.EntradaProduto.FindAsync(id);
+            if (entradaProduto == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProdutoId"] = new SelectList(_context.Produto, "Id", "Id", entradaProduto.ProdutoId);
+            return View(entradaProduto);
+        }
+
+        // POST: EntradaProduto/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,EntradaProdutoQuantidade,EntradaProdutoData,ProdutoId")] EntradaProduto entradaProduto)
+        {
+            if (id != entradaProduto.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(entradaProduto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EntradaProdutoExists(entradaProduto.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProdutoId"] = new SelectList(_context.Produto, "Id", "Id", entradaProduto.ProdutoId);
+            return View(entradaProduto);
+        }
+
+        // GET: EntradaProduto/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.EntradaProduto == null)
+            {
+                return NotFound();
+            }
+
+            var entradaProduto = await _context.EntradaProduto
+                .Include(e => e.Produto)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (entradaProduto == null)
+            {
+                return NotFound();
+            }
+
+            return View(entradaProduto);
+        }
+
+        // POST: EntradaProduto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.EntradaProduto == null)
+            {
+                return Problem("Entity set 'Contexto.EntradaProduto'  is null.");
+            }
+            var entradaProduto = await _context.EntradaProduto.FindAsync(id);
+            if (entradaProduto != null)
+            {
+                _context.EntradaProduto.Remove(entradaProduto);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EntradaProdutoExists(int id)
+        {
+          return (_context.EntradaProduto?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ projeto-final/Controllers/SaidaProdutoController.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using projeto_final.Models;
+
+namespace projeto_final.Controllers
+{
+    public class SaidaProdutoController : Controller
+    {
+        private readonly Contexto _context;
+
+        public SaidaProdutoController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: SaidaProduto
+        public async Task<IActionResult> Index()
+        {
+            var contexto = _context.SaidaProduto.Include(s => s.Cliente).Include(s => s.Produto).Include(s => s.TipoSaida).Include(s => s.Usuario);
+            return View(await contexto.ToListAsync());
+        }
+
+        // GET: SaidaProduto/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.SaidaProduto == null)
+            {
+                return NotFound();
+            }
+
+            var saidaProduto = await _context.SaidaProduto
+                .Include(s => s.Cliente)
+                .Include(s => s.Produto)
+                .Include(s => s.TipoSaida)
+                .Include(s => s.Usuario)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (saidaProduto == null)
+            {
+                return NotFound();
+            }
+
+            return View(saidaProduto);
+        }
+
+        // GET: SaidaProduto/Create
+        public IActionResult Create()
+        {
+            ViewData["ClienteId"] = new SelectList(_context.Cliente, "Id", "Id");
+            ViewData["ProdutoId"] = new SelectList(_context.Produto, "Id", "Id");
+            ViewData["TipoSaidaId"] = new SelectList(_context.TipoSaida, "Id", "Id");
+            ViewData["UsuarioId"] = new SelectList(_context.Usuario, "Id", "Id");
+            return View();
+        }
+
+        // POST: SaidaProduto/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,SaidaProdutoData,SaidaProdutoQuantidade,ProdutoId,UsuarioId,ClienteId,TipoSaidaId")] SaidaProduto saidaProduto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(saidaProduto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Cliente, "Id", "Id", saidaProduto.ClienteId);
+            ViewData["ProdutoId"] = new SelectList(_context.Produto, "Id", "Id", saidaProduto.ProdutoId);
+            ViewData["TipoSaidaId"] = new SelectList(_context.TipoSaida, "Id", "Id", saidaProduto.TipoSaidaId);
+            ViewData["UsuarioId"] = new SelectList(_context.Usuario, "Id", "Id", saidaProduto.UsuarioId);
+            return View(saidaProduto);
+        }
+
+        // GET: SaidaProduto/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.SaidaProduto == null)
+            {
+                return NotFound();
+            }
+
+            var saidaProduto = await _context.SaidaProduto.FindAsync(id);
+            if (saidaProduto == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Cliente, "Id", "Id", saidaProduto.ClienteId);
+            ViewData["ProdutoId"] = new SelectList(_context.Produto, "Id", "Id", saidaProduto.ProdutoId);
+            ViewData["TipoSaidaId"] = new SelectList(_context.TipoSaida, "Id", "Id", saidaProduto.TipoSaidaId);
+            ViewData["UsuarioId"] = new SelectList(_context.Usuario, "Id", "Id", saidaProduto.UsuarioId);
+            return View(saidaProduto);
+        }
+
+        // POST: SaidaProduto/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,SaidaProdutoData,SaidaProdutoQuantidade,ProdutoId,UsuarioId,ClienteId,TipoSaidaId")] SaidaProduto saidaProduto)
+        {
+            if (id != saidaProduto.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(saidaProduto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SaidaProdutoExists(saidaProduto.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Cliente, "Id", "Id", saidaProduto.ClienteId);
+            ViewData["ProdutoId"] = new SelectList(_context.Produto, "Id", "Id", saidaProduto.ProdutoId);
+            ViewData["TipoSaidaId"] = new SelectList(_context.TipoSaida, "Id", "Id", saidaProduto.TipoSaidaId);
+            ViewData["UsuarioId"] = new SelectList(_context.Usuario, "Id", "Id", saidaProduto.UsuarioId);
+            return View(saidaProduto);
+        }
+
+        // GET: SaidaProduto/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.SaidaProduto == null)
+            {
+                return NotFound();
+            }
+
+            var saidaProduto = await _context.SaidaProduto
+                .Include(s => s.Cliente)
+                .Include(s => s.Produto)
+                .Include(s => s.TipoSaida)
+                .Include(s => s.Usuario)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (saidaProduto == null)
+            {
+                return NotFound();
+            }
+
+            return View(saidaProduto);
+        }
+
+        // POST: SaidaProduto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.SaidaProduto == null)
+            {
+                return Problem("Entity set 'Contexto.SaidaProduto'  is null.");
+            }
+            var saidaProduto = await _context.SaidaProduto.FindAsync(id);
+            if (saidaProduto != null)
+            {
+                _context.SaidaProduto.Remove(saidaProduto);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SaidaProdutoExists(int id)
+        {
+          return (_context.SaidaProduto?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
